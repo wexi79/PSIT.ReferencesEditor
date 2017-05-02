7230fadb32e20db01ebd8f6fb072d1e9201d6737
Veikko Skurnik <veikko.skurnik@psit.fi>
2017-05-02 04:03:11 +0300

    Initial commit

---
+++ Classes/Package.php
@@ -0,0 +1,12 @@
+<?php
+namespace PSIT\ReferencesEditor;
+
+use Neos\ContentRepository\Domain\Model\Node;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\Flow\Core\Bootstrap;
+use Neos\Flow\Package\Package as BasePackage;
+
+class Package extends BasePackage
+{
+
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# PSIT.ReferencesEditor
+
+A node references editor that uses two side-by-side lists, useful for dealing with nodes with large amounts of references
